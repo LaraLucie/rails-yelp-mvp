9d783ee3d1708bce51dbec142b321b27b8e6a559
Lara Flanderka <lflanderka@gmail.com>
2022-05-18 16:01:13 +0200

    wrote the restaurants controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+puts 'Start'
+5.times do
+  Restaurant.create({
+    'name' => Faker::Games::Pokemon.name,
+    'address' => Faker::JapaneseMedia::OnePiece.island,
+    'phone_number' => Faker::TvShows::BrooklynNineNine.character,
+    'category' => %w[chinese italian japanese french belgian].sample
+  })
+end
+puts 'End'
